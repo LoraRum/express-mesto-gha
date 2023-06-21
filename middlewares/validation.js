91c400026639d1663fdab80728117ddf5b58b4b4
Laura Rumiantseva <gromina.l.d@gmail.com>
2023-06-21 15:39:44 +0600

    валидация ссылок

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,4 +1,17 @@
 const { celebrate, Joi } = require('celebrate');
+const { URL } = require('url');
+
+const urlValidator = (value, helpers) => {
+  try {
+    const url = new URL(value);
+    if (url.protocol !== 'http:' && url.protocol !== 'https:') {
+      throw new Error('Invalid URL scheme');
+    }
+    return value;
+  } catch (error) {
+    throw helpers.error('any.invalid', { message: 'Invalid URL' });
+  }
+};
 
 const validateLogin = celebrate({
   body: Joi.object().keys({
@@ -11,35 +24,31 @@ const validateCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
+    avatar: Joi.string().custom(urlValidator),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
-const validateUpdateProfile = celebrate(
-  {
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  },
-);
-
-const validateUpdateAvatar = celebrate({
+const validateUpdateProfile = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 });
 
-const validateCreateCard = celebrate(
-  {
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30).required(),
-      link: Joi.string().required().uri({ scheme: ['http', 'https'] }),
-    }),
-  },
-);
+const validateUpdateAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().custom(urlValidator),
+  }),
+});
+
+const validateCreateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().custom(urlValidator),
+  }),
+});
 
 const validateUserId = celebrate({
   params: Joi.object().keys({
